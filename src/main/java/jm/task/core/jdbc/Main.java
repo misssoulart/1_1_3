24b8282283802fbd7cf406a17f0a67d2c09bb88e
Ekaterina Yakovenko <katya-sitnova@yandex.ru>
2022-09-05 13:19:12 +0300

    commit_05.09.22

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,16 +8,14 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 import java.sql.SQLException;
 
 public class Main {
-    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
         UserDao userDao = new UserDaoJDBCImpl();
 
-        /*
         userDao.saveUser("Иван", "Иванов", (byte) 20);
         userDao.saveUser("Петр", "Сидоров", (byte) 30);
         userDao.saveUser("Ольга", "Коновалова", (byte) 40);
-        userDao.saveUser("Мария", "Кошкина", (byte) 50); */
+        userDao.saveUser("Мария", "Кошкина", (byte) 50);
 
     }
 }
